949ceeb09e8994ad4c7b4dee0977ec50f3adc50f
Администратор <@microsof-f1e706>
2014-08-10 22:48:30 +0400

    njjjjj

---
+++ BasicStrust2ProjectEclipse/src/com/tutorials4u/actions/LinkAction.java
@@ -0,0 +1,28 @@
+package com.tutorials4u.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LinkAction extends ActionSupport {
+
+	private static final long serialVersionUID = 1L;
+
+	
+	@Override
+	public String execute() throws Exception {
+		return "success";
+	}
+	public String welcome()
+	{
+		return "welcome";		
+	}
+	
+	public String struts()
+	{
+		return "struts";		
+	}
+	
+	public String hibernate()
+	{
+		return "hibernate";		
+	}
+}
